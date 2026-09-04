85dc35eca5fc85b1d88ffb3b1fd7e3ae1ad861ed
saboracaiteria <saboracaiteria@users.noreply.github.com>
2026-09-04 14:27:52 -0300

    chore: atualiza PORT para process.env.PORT para deploy no Render

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const DEFAULT_MAP = 0; // Burg
 
 // HEADERS ANTI-CACHE & CORS
